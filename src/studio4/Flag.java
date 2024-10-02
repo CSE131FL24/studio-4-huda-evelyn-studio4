f0533d14d98fa58c75e61354485fdd850649a373
hudaa <hudaa@HudaDell.dhcp.wustl.edu>
2024-10-02 17:19:25 -0500

    done cursed spongbob

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,22 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledRectangle(0.5,0.5,0.22,0.26);
 		
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledCircle(0.38,0.54,0.07);
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledCircle(0.62,0.54,0.07);
+		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledCircle(0.38,0.54,0.04);
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledCircle(0.62,0.54,0.04);
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledCircle(0.38,0.54,0.025);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledCircle(0.62,0.54,0.025);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,30 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
-	}
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfwidth = in.nextDouble();
+		double halfheight = in.nextDouble();
+		StdDraw.setPenColor (redComponent,greenComponent,blueComponent);
+		if (shapeType.equals("rectangle")) {
+			if (isFilled) {
+				StdDraw.filledRectangle(x,y,halfwidth,halfheight);
+			}
+				
+			else {
+				StdDraw.rectangle(x,y,halfwidth,halfheight);
+			}
+		}
+			
+		
+	} 
+	
 }
